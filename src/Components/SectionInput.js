a912d2d99a6ba15987bcc702b866029a250c7e66
rezabox <reza.asareh81@gmail.com>
2023-04-06 12:55:45 +0430

    add input section

--- src/Components/SectionInput.js
+++ src/Components/SectionInput.js
@@ -1,24 +1,28 @@
 function SectionInput() {
   return (
-    <div className="container text-center mt-20">
-      <div className="row">
+    <div className="w-[100%] mt-20 text-center">
+      <div className="row m-0">
         <h1 className="sub_title text-4xl text-color-sub-title mb-[25px] mt-[60px]">
           بزرگترین فروشگاه قالب و افزونه وردپرس در ایران
         </h1>
-        <h2 className="down_title text-2xl text-down-title mb-[40px]">
-          راهکار مطمئن و<b className="title_word text-green-oil">سریع</b>
+        <h2 className="down_title  text-2xl text-down-title mb-[40px]">
+          راهکار مطمئن و<span className="title_word text-green-oil">سریع </span>
           برای طراحی و توسعه وب سایت
         </h2>
-        <form className="mr-[30%] ml-[30%]">
-          <div className="mb-6 md:grid-cols-2">
-            <div>
-              <input
-                type="text"
-                id="first_name"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="اسم قالب یا افزونه شما مثلاً: انفولد"
-                required
-              />
+        <form className="mr-[20%] ml-[22%]">
+          <div className="mt-8 flex ">
+            <input
+              type="text"
+              id="first_name"
+              className="text-color-input relative shadow-shadowInput text-sm rounded-sm block w-full p-2.5 outline-none"
+              placeholder="اسم قالب یا افزونه شما مثلاً: انفولد"
+              required
+            />
+            <button className="mb-4  h-[2vh]">
+              <i className="bi bi-search hover:bg-hoverSearch-color transition-colors  mr-[60%] mt-[40%] text-xl bg-green-oil py-2 px-4 rounded-sm text-white"></i>
+            </button>
+            <div className="option absolute flex h-9 mt-1 mr-1">
+              <button className="product text-base px-4 text-color-selectBox bg-back-selectBox rounded-sm  ">همه محصولات<span className="text-sm"><i class="bi bi-chevron-down mr-2"></i></span></button>
             </div>
           </div>
         </form>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
           'green-oil': '#8ed557',
           'color-sub-title':'#6d6d6d',
           'down-title':'#3c4354',
+          'color-input':'#6a6a6a',
+          'nav-color': '#abb8c3',
+          'hoverSearch-color':'rgba(100,207,76,.94)',
+          'color-selectBox':'#c9c9c9',
+          'back-selectBox':'#f1f1f1;',
+      },
+      boxShadow: {
+          'shadowInput':'0 5px 25px rgba(0,0,0,.1)',
+          'shadowSearch':'0 5px 15px rgba(126,207,66,.34)',
       }
     },
   },
